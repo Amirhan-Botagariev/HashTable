de457aa7321cdbe7685068e7271cc551d297c75e
Amirhan-Botagariev <amirkhan1botagariev@mail.ru>
2023-05-15 00:43:03 +0600

    Created hashtable and put into it 10000 elements.

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        HashTable hashTable = new HashTable<MyTestingClass, String>();
+        Random rand = new Random();
+        for (int i = 0; i < 10000; i++){
+            int value = rand.nextInt(0, 9999999);
+            hashTable.put(new MyTestingClass(value), "Student " + value);
         }
+        System.out.println(hashTable.getSize());
     }
 }
